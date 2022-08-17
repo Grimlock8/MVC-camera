77ffa81d2ea89d97cec7687f446a3a7b187a114e
Grimlock8 <105064843+Grimlock8@users.noreply.github.com>
2022-08-17 13:24:28 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CameraTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
